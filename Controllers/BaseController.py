213c5758d42cd97f89bb26cbd879c2f3e623d18e
mohamedfathi540 <mohamed.fathe540@gmail.com>
2026-01-02 21:47:23 +0200

    feat: Add vector database configuration, refactor Qdrant provider, clean up asset files, and enhance database path management.

--- Controllers/BaseController.py
+++ Controllers/BaseController.py
@@ -19,5 +19,19 @@ class basecontroller :
             "Assets/Files"
         )
 
+
+
+        self.database_dir = os.path.join(self.base_dir,"Assets/Database")
+
     def generate_random_string (self, length : int=12) :
         return ''.join(random.choices(string.ascii_lowercase + string.digits ,k = length))
+
+
+    def get_database_path(self, db_name : str) :
+        database_path = os.path.join(self.database_dir  , db_name)
+
+        if not os.path.exists(database_path) :
+            os.makedirs(database_path)
+
+        return database_path
+    
