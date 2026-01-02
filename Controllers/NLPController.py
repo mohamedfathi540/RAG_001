30a9f1bf2d24a23a9ddc146c31ad21473b4e620a
mohamedfathi540 <mohamed.fathe540@gmail.com>
2026-01-02 23:30:12 +0200

    feat: Implement vector database collection management in NLPController and refactor `Project` schema casing with typo fixes.

--- Controllers/NLPController.py
+++ Controllers/NLPController.py
@@ -1,5 +1,6 @@
 from .BaseController import basecontroller
 from .ProjectController import projectcontroller
+from Models.DB_Schemes import Project
 from fastapi import UploadFile
 from Models import ResponseSignal
 import re
@@ -11,3 +12,21 @@ class NLPController (basecontroller) :
 
     def __init__(self ,genration_client ,embedding_client ,vectordb_client) :
         super().__init__()
+        self.genration_client = genration_client
+        self.embedding_client = embedding_client
+        self.vectordb_client = vectordb_client  
+
+
+    def create_collection_name (self , project_id  : str) :
+        return f"collection_{project_id}".strip()
+
+
+    def reset_vector_db_collection (self , project : Project) :
+        collection_name = self.create_collection_name(project_id = project.project_id)
+        return self.vectordb_client.delete_collection(collection_name = collection_name)
+
+    def get_vector_collection_info ( self , project : Project) :
+        collection_name = self.create_collection_name(project_id = project.project_id)
+        collection_info = self.vectordb_client.get_collection_info(collection_name = collection_name)
+        return collection_info
+       
